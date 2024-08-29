2a1e1561462cbc5667267252b518ef863b4dac56
Syed41 <junaid459561@gmail.com>
2024-08-30 02:29:41 +0500

    Firebase Configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:opatra/UI/Splash%20Screen.dart';
 import 'constant/AppColors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
